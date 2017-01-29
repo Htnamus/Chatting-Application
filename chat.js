bc0784df73d8d0f4ba15091bfd0e99d51b788c8e
Htnamus <tangiralasumanth@gmail.com>
2017-01-30 00:34:32 +0530

    finishing almost

--- chat.js
+++ chat.js
@@ -11,4 +11,21 @@ ifUserIsLoggedIn(function(){
 		}
 		getElement("chat_member").innerHTML=usersList;
 	});
+	onClickMultiple("chat_member", function(element){
+		var chat_id=element.id;
+		loadMessages(chat_id,function(messages){
+			var messagesList="";
+			for(var uid in messages){
+				var message=messages[uid];
+				messagesList+=renderMessage(message);
+			}
+			getElement("messages").innerHTML=messagesList
+		});
+		getElement("chat-id").value=chat-id;
+	});
+	click("send-button",function(){
+		var text=getElement("message-text").value;
+		var chat_id=getElement("chat-id").value;
+		sendMessage(chat_id,text);
+	});
 });
--- functions.js
+++ functions.js
@@ -2,7 +2,6 @@ function click(elementId,fn){
 	var element = document.getElementById(elementId);
 	if(element){
 		element.addEventListener("click", fn);
-
 	}
 }
 function redirect(path){
@@ -64,7 +63,6 @@ function updateUserData(){
 	var usernameElement = getElement("username");
 	usernameElement.textContent = window.currentUser.name;
 }
-
 function loadUsers(fn){
 	var database =firebase.database();
 	var usersRef=database.ref("users");
@@ -78,7 +76,7 @@ function renderUser(user){
 	var chat_id=getChatId(window.currentUser.id,uid);
 	console.log(uid);
 	var name=user.name;
-	var html = '<div id="' + chat_id + '" class="members" >' + name + '</div>';
+	var html = '<div id="' + chat_id + '" class="chat_member" >' + name + '</div>';
 	return html;
 }
 function getChatId(id1,id2) {
@@ -87,4 +85,48 @@ function getChatId(id1,id2) {
 	}
 	else
 		return id2+""+id1;
+}
+function onClickMultiple(className,func) {
+    document.addEventListener('click', function(event) {
+        if (event.target.classList.contains(className)) {
+            func(event.target);
+        }
+    });
+}
+function loadMessages(chat_id,fn){
+	var database =firebase.database();
+	var usersRef=database.ref("users");
+	usersRef.on('value',function(snapshot){
+		var database =firebase.database();
+		var chatsRef=database.ref("chats");
+		var chats
+		chatsRef.child(chat_id).on('value',function(snapshot){
+			var messages=snapshot.val();
+			fn(messages);
+		});
+	});
+}
+function renderMessage(message){
+	var text=message.text;
+	var msgClass="message";
+
+	if(message.sender_id==window.currentUser.id){
+		msgClass="message_by_me";
+	}
+	var html='<div class='+msgClass+'">'+text+'</div>';
+	return html;
+}
+function sendMessage(chat_id,text) {
+	var message={
+		text:text,
+		sender_id:window.currentUser.id,
+	}
+
+	var database= firebase.database();
+	var chatsRef=database.ref("chats");
+	var char =chatsRef.child(chat_id);
+	var newMessage=chatsRef.push().key;
+
+	chat.child(newMessageId).set(message);
+
 }
